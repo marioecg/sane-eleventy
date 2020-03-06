c549770c6725da94689b24e0ec28d947963e8c03
marioecg <marioecg@gmail.com>
2020-03-06 14:37:47 -0700

    added highway folder

---
+++ web/src/scripts/highway/transitions/Default.js
@@ -0,0 +1,27 @@
+import Highway from '@dogstudio/highway'
+
+class DefaultTransition extends Highway.Transition {
+  in({ from, to, done }) {
+    gsap.timeline().to('.js-mask', {
+      duration: 0.35,
+      autoAlpha: 0,
+      ease: 'linear',
+    })
+
+    from.remove()
+    done()
+  }
+
+  out({ done }) {
+    gsap.to('.js-mask', {
+      duration: 0.35,
+      autoAlpha: 1,
+      ease: 'linear',
+      onComplete: () => {
+        done()
+      },
+    })
+  }
+}
+
+export default DefaultTransition
